24299c3b98416bb39a488dcbc6470b716767d71f
Nasko Vasilev <nasko.vasilev@betvictor.com>
2015-10-19 16:45:33 +0200

    Action Sheets and ion-overlay

--- www/app/about/about.ts
+++ www/app/about/about.ts
@@ -1,4 +1,4 @@
-import {Page} from 'ionic/ionic';
+import {Page, ActionSheet} from 'ionic/ionic';
 import {Friends} from '../data/data';
 import {Control, ControlGroup} from 'angular2/angular2';
 
@@ -6,10 +6,39 @@ import {Control, ControlGroup} from 'angular2/angular2';
   templateUrl:"app/about/about.html",
 })
 export class AboutCtrl {
-  constructor(friends:Friends){
+  constructor(friends:Friends, actionSheet:ActionSheet){
     this.settings = new ControlGroup({
       enableFriends: new Control(true)
     });
+    this.actionSheet = actionSheet;
+  }
+
+  showActionSheet() {
+    this.actionSheet.open({
+      buttons: [
+        { text: 'Share This' },
+        { text: 'Move' }
+      ],
+      destructiveText: 'Delete',
+      titleText: 'Modify your album',
+      cancelText: 'Cancel',
+      cancel: function() {
+        // add cancel code..
+        console.log('Canceled');
+      },
+      destructiveButtonClicked: () => {
+        console.log('Destructive clicked');
+      },
+      buttonClicked: function(index) {
+        console.log('Button clicked', index);
+        if(index == 1) { return false; }
+        return true;
+      }
+    }).then(actionSheet => {
+      // Action sheet was created and opened
+      // this.actionSheet = actionSheet;
+      // this.actionSheet.close() to close it programatically
+    })
   }
 }
 
